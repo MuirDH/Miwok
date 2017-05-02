7bbe19ff86c29161241da9b5f7687619cca656a0
Muir Halleron <muir.halleron@gmail.com>
2017-05-02 12:15:28 +0100

    Change Array words to ArrayList words

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -14,30 +16,30 @@ public class NumbersActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Create a String array with 10 indices containing the words for the numbers 1-10
-        String words[] = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        //Create an ArrayList containing the words for the numbers 1-10
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
         // Log messages to check that the array has been initialised correctly
-        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[2]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[3]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[4]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[5]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[6]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[7]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[8]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[9]);
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(1));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(2));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(3));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(4));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(5));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(6));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(7));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(8));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(9));
 
     }
 
