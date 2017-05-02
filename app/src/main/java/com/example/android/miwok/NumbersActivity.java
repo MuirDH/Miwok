a9cfed00cf57d8de6fc9ae88e547cbbbada24a6a
Muir Halleron <muir.halleron@gmail.com>
2017-05-02 11:41:03 +0100

    Create verbose log to check that the words array is working

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -13,9 +14,7 @@ public class NumbersActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /**
-         * Create a String array with 10 indices containing the words for the numbers 1-10
-         */
+        //Create a String array with 10 indices containing the words for the numbers 1-10
         String words[] = new String[10];
         words[0] = "one";
         words[1] = "two";
@@ -27,6 +26,18 @@ public class NumbersActivity extends AppCompatActivity {
         words[7] = "eight";
         words[8] = "nine";
         words[9] = "ten";
+
+        // Log messages to check that the array has been initialised correctly
+        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[2]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[3]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[4]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[5]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[6]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[7]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[8]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[9]);
 
     }
 
