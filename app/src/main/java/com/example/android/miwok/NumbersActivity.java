32e768e7b016abbfebb37fb395915d9c9fc9d6ef
Muir Halleron <muir.halleron@gmail.com>
2017-05-02 11:28:58 +0100

    Create an array for number words 1-10

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -13,6 +13,21 @@ public class NumbersActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /**
+         * Create a String array with 10 indices containing the words for the numbers 1-10
+         */
+        String words[] = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
     }
 
 }
