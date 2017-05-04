c15d9ea7786f8900360f3eb9a61d90b45d74eaee
Muir Halleron <muir.halleron@gmail.com>
2017-05-04 11:52:54 +0100

    Impliment code in all Activities

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -3,15 +3,51 @@ package com.example.android.miwok;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Create an ArrayList containing the words for the colours
+        ArrayList<Word> words = new ArrayList<>();
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        /*
+         Array adapter whose data source is a list of Strings. The list item layout, which is a
+         resource defined in the Android framework, contains a single TextView which the adapter
+         will set to display a single word.
+         */
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        /*
+         Find the ListView object in the view hierarchy of the Activity.
+         */
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        /*
+         Make the ListView use the ArrayAdapter we created above, so that the ListView will display
+         list items for each word in the list of words. This is done by calling the setAdapter
+         method on the Listview object and passing in 1 argument, which is the ArrayAdapter with the
+         variable name itemsAdapter.
+         */
+        listView.setAdapter(adapter);
+
 
     }
 
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,6 @@ package com.example.android.miwok;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -13,7 +12,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
